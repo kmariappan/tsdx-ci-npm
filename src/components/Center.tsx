861081a4621d878157740b71acbb39050c265ea3
Karthikeyan Mariappan <karthikeyanmariappan@hotmail.com>
2021-10-16 16:48:10 +0200

    fix(layout component): heith alignment for the Center Component

--- src/components/Center.tsx
+++ src/components/Center.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react'
 
 export const Center:FC = ({children}) => {
     return (
-        <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+        <div style={{display:'flex', justifyContent:'center', alignItems:'center', minHeight:'100vh', height:'100vh', padding:'0 0.5rem'}}>
             {children}
         </div>
     )
