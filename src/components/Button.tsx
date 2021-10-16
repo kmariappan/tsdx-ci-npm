f902efab70179a533eb13626ac15c6de829dd7f7
Karthikeyan Mariappan <karthikeyanmariappan@hotmail.com>
2021-10-16 17:00:05 +0200

    feat(layout component): new Button Component added

---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Button = () => {
+    return (
+        <button style={{minWidth:'200px', minHeight:'150px'}}>
+            Hello
+        </button>
+    )
+}
